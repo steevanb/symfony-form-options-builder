80185e7e803a341f0e4b7ed17444037125c40d21
Steevan BARBOYON <steevan.barboyon@gmail.com>
2015-07-31 15:36:07 +0200

    Add CascadeValidation trait

---
+++ Fields/Traits/CascadeValidation.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace steevanb\FormUtils\Fields\Traits;
+
+trait CascadeValidation
+{
+    use ParameterAccessors;
+
+    /**
+     * @param $validation
+     * @return $this
+     * @link http://symfony.com/doc/current/reference/forms/types/collection.html#cascade-validation
+     */
+    public function setCascadeValidation($validation)
+    {
+        return $this->setParameter('cascade_validation', boolval($validation));
+    }
+
+    /**
+     * @param bool $default
+     * @return bool
+     */
+    public function getCascadeValidation($default = false)
+    {
+        return $this->getParameter('cascade_validation', $default);
+    }
+}
