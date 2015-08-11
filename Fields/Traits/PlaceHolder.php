0579d5654372cfc7a2c43cfc9ab06feb5ad22c6c
Steevan BARBOYON <steevan.barboyon@gmail.com>
2015-08-11 14:55:01 +0200

    Add addAttr() and getAttr() as abstract

--- Fields/Traits/PlaceHolder.php
+++ Fields/Traits/PlaceHolder.php
@@ -7,6 +7,19 @@ trait PlaceHolder
     use ParameterAccessors;
 
     /**
+     * @param string $name
+     * @param mixed $value
+     * @return $this
+     */
+    abstract public function addAttr($name, $value);
+
+    /**
+     * @param string $name
+     * @return array|mixed
+     */
+    abstract public function getAttr($name = null);
+
+    /**
      * @param false|string $placeholder
      * @return $this
      * @link http://symfony.com/doc/current/reference/forms/types/choice.html#placeholder
