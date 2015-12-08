9c01300b84b874a2487665c5d6dc0eed9b3f220c
Steevan BARBOYON <steevan.barboyon@gmail.com>
2015-12-08 09:51:56 +0100

    Add getBlockPrefix() trait for 2.8

---
+++ BlockPrefixTrait.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace steevanb\FormUtils;
+
+trait BlockPrefixTrait
+{
+    /**
+     * @return string
+     * @link https://github.com/symfony/symfony/pull/16724
+     * @since 2.8
+     */
+    public function getBlockPrefix()
+    {
+        $fullClassName = get_class($this);
+        $className = ($pos = strrpos($fullClassName, '\\')) ? substr($fullClassName, $pos + 1) : $fullClassName;
+        $className = ($pos = strrpos($className, 'Type')) ? substr($className, 0, -4) : $className;
+
+        return 'form_type_' . strtolower($className);
+    }
+}
