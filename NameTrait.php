17bb983ce3fc091044af1738e1cc11463950716a
Steevan BARBOYON <steevan.barboyon@gmail.com>
2015-12-08 09:51:22 +0100

    Deprecated since 2.8

--- NameTrait.php
+++ NameTrait.php
@@ -2,10 +2,16 @@
 
 namespace steevanb\FormUtils;
 
+/**
+ * @link https://github.com/symfony/symfony/blob/2.8/UPGRADE-2.8.md#form
+ * @deprecated Since 2.8
+ */
 trait NameTrait
 {
     /**
      * @return string
+     * @link https://github.com/symfony/symfony/blob/2.8/UPGRADE-2.8.md#form
+     * @deprecated Since 2.8
      */
     public function getName()
     {
