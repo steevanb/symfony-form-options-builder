a0ad8ef0e7a4c079afde8ae24057077699a77971
Steevan BARBOYON <steevan.barboyon@gmail.com>
2016-06-09 14:57:18 +0200

    Add FixDisabledBugOptionsBuilder

---
+++ OptionsBuilder/FixDisabledBugOptionsBuilder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace steevanb\SymfonyFormOptionsBuilder\OptionsBuilder;
+
+/**
+ * Use it to fix disabled configuration bug :
+ * If all your fields are disabled, your browser doesn't send anything
+ * In this case, Symfony understand that you want to delete element, instead of do not modify it.
+ * You have to call form_widget(form.foo) in your template to write this input in HTML
+ */
+class FixDisabledBugOptionsBuilder extends HiddenOptionsBuilder
+{
+    public function __construct()
+    {
+        $this
+            ->setMapped(false)
+            ->setLabel(false)
+            ->setTranslationDomain(false);
+    }
+}
